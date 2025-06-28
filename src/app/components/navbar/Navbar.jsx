e5fea5eee54b4a26bcd0ca8ded4b43ea85402af8
Jordan Schulte <email.jordanschulte@gmail.com>
2025-06-28 17:51:02 -0400

    Created nav menu

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import Link from "next/link"
 import Image from "next/image"
 
+import './navbar.css'
+
 const Navbar = () => {
   return (
-    <div className="nav-inner">
+    <div className="nav-inner flex">
+      <div className="nav-left flex">
         <div className="nav-logo">
-            
+          <p>Rank Junkie</p>
         </div>
+        <ul className="nav-links flex">
+          <li><Link href="#">Home</Link></li>
+          <li><Link href="#">How It Works</Link></li>
+          <li><Link href="#">Rankings</Link></li>
+          <li><Link href="#">Contact</Link></li>
+        </ul>
+      </div>
+      <div className="nav-right">
+        <div className="burger flex-center">
+          <div className="line line1"></div>
+          <div className="line line2"></div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import Navbar from "@/app/components/navbar/Navbar";
+
 export const metadata = {
   title: "Rank Junkie",
   description: "Fantasy rankings app by Jordan Schulte",
@@ -9,7 +11,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <nav>
+          <Navbar />
+        </nav>
+        <div className="content-wrapper">
+          {children}
+        </div>
       </body>
     </html>
   );
