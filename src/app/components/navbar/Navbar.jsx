dd0192f2dad53c3cc1e0a0de794e1c72b8385d95
Jordan Schulte <email.jordanschulte@gmail.com>
2025-06-28 14:19:39 -0400

    Set up

---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const Navbar = () => {
+  return (
+    <div className="nav-inner">
+        <div className="nav-logo">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
