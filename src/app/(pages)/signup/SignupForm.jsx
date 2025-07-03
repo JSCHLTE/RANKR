ea2f546de0bc110f0d3b25a257738162e2bb2c0f
Jordan Schulte <email.jordanschulte@gmail.com>
2025-07-03 10:44:16 -0400

    Started adding rules for signup

--- src/app/(pages)/signup/SignupForm.jsx
+++ src/app/(pages)/signup/SignupForm.jsx
@@ -12,6 +12,7 @@ const SignupForm = () => {
     });
 
     const handleInput = (event) => {
+
         const { name, value } = event.target;
 
         setUserInfo(prev => ({
@@ -22,23 +23,24 @@ const SignupForm = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        if(userInfo.password !== userInfo.confirmPassword) alert("Passwords do not match");
       }
 
   return (
     <form id='loginForm' className="flex" onSubmit={handleSubmit}>
-    <label className="flex">
+    <label className="flex" htmlFor='email'>
         Email:
         <input type='email' placeholder="Email..." name='email' onChange={handleInput} value={userInfo.email}/>
     </label>
-    <label className="flex">
+    <label className="flex" htmlFor='username'>
         Username:
         <input type='text' placeholder="Username..." name='username' onChange={handleInput} value={userInfo.username}/>
     </label>
-    <label className="flex">
+    <label className="flex" htmlFor='password'>
         Password:
         <input type="password" placeholder="Password..." name='password' onChange={handleInput} value={userInfo.password}/>
     </label>
-    <label className="flex">
+    <label className="flex" htmlFor='confirmPassword'>
         Confirm Password:
         <input type="password" placeholder="Confirm Password..." name='confirmPassword' onChange={handleInput} value={userInfo.confirmPassword}/>
     </label>
