87c4249d05906a9f14015d64acd5786377100ef3
Jordan Schulte <email.jordanschulte@gmail.com>
2025-10-23 16:01:18 -0400

    quick component fix

--- src/app/components/close/Close.jsx
+++ src/app/components/close/Close.jsx
@@ -1,6 +1,6 @@
 import "./close.css"
 
-export default function Close ({ onClick, className = '', style = {} }) => {
+export default function Close ({ onClick, className = '', style = {} }) {
     <div onClick={onClick} style={style} className={className}>
         <i className="fa-solid fa-x"></i>
     </div>
