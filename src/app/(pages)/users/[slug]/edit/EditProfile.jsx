a71bdaa07fa20146a03ad201326ea1f848730df9
Jordan Schulte <email.jordanschulte@gmail.com>
2025-11-14 16:05:59 -0500

    edit page

---
+++ src/app/(pages)/users/[slug]/edit/EditProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./edit.module.css"
+
+const EditProfile = () => {
+  return (
+    <div className={styles.editWrapper}>
+
+    </div>
+  )
+}
+
+export default EditProfile
