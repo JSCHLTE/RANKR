6fce585ea633706b77f2eb2b327133aed84e5055
Jordan Schulte <email.jordanschulte@gmail.com>
2025-12-22 12:01:58 -0500

    Added drag tab css into playeritem.css

--- src/app/(pages)/rankings/[slug]/PlayerItem.jsx
+++ src/app/(pages)/rankings/[slug]/PlayerItem.jsx
@@ -3,10 +3,10 @@ import "../../CSS/playerItem.css"
 import "../../CSS/playerTable.css"
 import PlayerTable from "./PlayerTable";
 
-const PlayerItem = ({ 
-  player, 
-  overallRank, 
-  onClick, 
+const PlayerItem = ({
+  player,
+  overallRank,
+  onClick,
   playerList,
   dragHandleProps, // For drag handle in edit mode
   isDragging, // For drag-specific styles
@@ -18,62 +18,62 @@ const PlayerItem = ({
   const playerSeasons = playerCard?.find(obj => obj.playerId === player.playerId);
 
   return (
-    <div 
-      className={`player-item ${playerCard?.some(obj => obj.playerId === player.playerId) ? 'active' : ''}`}  
+    <div
+      className={`player-item ${playerCard?.some(obj => obj.playerId === player.playerId) ? 'active' : ''}`}
       style={{
         zIndex: isDragging ? 1000 : 'auto',
         position: isDragging ? 'fixed' : 'static',
       }}
     >
       <div className="player-item-inner">
-      <div className="player-item-all-wrapper">
-      <div className="player-item-header flex" onClick={onClick}>
-      {dragHandleProps && ( // Conditionally render drag handle for edit mode
-        <div className="drag-handle-tab flex-center" {...dragHandleProps}>
-          <div className="lines-wrapper flex-center">
-            <div className="line-drag"></div>
-            <div className="line-drag"></div>
-            <div className="line-drag"></div>
-          </div>
-        </div>
-      )}
-      <div className="player-item-img-wrapper">
-          {/* {player.currentTeam && ( // Assuming player has currentTeam; make optional if needed
+        <div className="player-item-all-wrapper">
+          <div className="player-item-header flex" onClick={onClick}>
+            {dragHandleProps && ( // Conditionally render drag handle for edit mode
+              <div className="drag-handle-tab flex-center" {...dragHandleProps}>
+                <div className="lines-wrapper flex-center">
+                  <div className="line-drag"></div>
+                  <div className="line-drag"></div>
+                  <div className="line-drag"></div>
+                </div>
+              </div>
+            )}
+            <div className="player-item-img-wrapper">
+              {/* {player.currentTeam && ( // Assuming player has currentTeam; make optional if needed
             <img
               src={`https://sleepercdn.com/images/team_logos/nfl/${player.currentTeam.toLowerCase()}.png`}
               alt={`${player.currentTeam} logo`}
               className="player-team-logo"
             />
           )} */}
-          <img 
-            src={player.playerImg ?? "/images/player-default.webp"} 
-            alt={player.full_name}
-            onError={(e) => {
-              e.currentTarget.onerror = null;
-              e.currentTarget.src = "/images/player-default.webp";
-            }} 
-          />
-        </div>
-        <div className="player-item-info-wrapper">
-          <div className="player-item-info-name">
-            <div className="player-name-wrapper"><p className="player-name">{player.full_name ? player.full_name : "N/A"}</p> {player.years_exp === 0 && player.position !== "DEF" ? (
-              <div className="player-rookie"><img src="/images/rookie.svg" draggable="false"/></div>
-            ) : null}</div>
-          </div>
-          <div className="player-item-info-details flex">
-            <span>OVR: {overallRank ? overallRank : "N/A"}</span>
-            <span className={`player-pos ${player.position ? player.position : "N/A"}`}>
-              {player.position ? player.position : "N/A"} {check(playerList, player)}
-            </span>
-            {player.currentTeam ? (
-            <span style={{backgroundColor: `var(--${player.currentTeam.toLowerCase()})`, borderColor: `var(--${player.currentTeam.toLowerCase()})`}}>{player.currentTeam}</span>
-            ) : null}
+              <img
+                src={player.playerImg ?? "/images/player-default.webp"}
+                alt={player.full_name}
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = "/images/player-default.webp";
+                }}
+              />
+            </div>
+            <div className="player-item-info-wrapper">
+              <div className="player-item-info-name">
+                <div className="player-name-wrapper"><p className="player-name">{player.full_name ? player.full_name : "N/A"}</p> {player.years_exp === 0 && player.position !== "DEF" ? (
+                  <div className="player-rookie"><img src="/images/rookie.svg" draggable="false" /></div>
+                ) : null}</div>
+              </div>
+              <div className="player-item-info-details flex">
+                <span>OVR: {overallRank ? overallRank : "N/A"}</span>
+                <span className={`player-pos ${player.position ? player.position : "N/A"}`}>
+                  {player.position ? player.position : "N/A"} {check(playerList, player)}
+                </span>
+                {player.currentTeam ? (
+                  <span style={{ backgroundColor: `var(--${player.currentTeam.toLowerCase()})`, borderColor: `var(--${player.currentTeam.toLowerCase()})` }}>{player.currentTeam}</span>
+                ) : null}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-</div>
-      { playerCard?.some(obj => obj.playerId === player.playerId) ? <PlayerTable key={player} player={player} playerSeasons={playerSeasons} playerCard={playerCard} setPlayerCard={setPlayerCard} loading={loading} /> : ""}
+      {playerCard?.some(obj => obj.playerId === player.playerId) ? <PlayerTable key={player} player={player} playerSeasons={playerSeasons} playerCard={playerCard} setPlayerCard={setPlayerCard} loading={loading} /> : ""}
     </div>
   );
 };
